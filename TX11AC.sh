acfa3c16a9adaf2d527335ee07a17d16a426bab6
Atopes Sayuri <108107785+AtopesSayuri@users.noreply.github.com>
2024-05-04 14:43:25 +0800

    Partially completed

--- TX11AC.sh
+++ TX11AC.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 #By Atopes
-
-# ENV
-X11APKURL="https://github.com/termux/termux-x11/releases/download/latest/app-arm64-v8a-debug.apk"
-PROJREPO="https://github.com/AtopesSayuri/TermuxX11AutoConfig"
-PROJRAWURL="https://github.com/AtopesSayuri/TermuxX11AutoConfig/raw/main"
-# Often used func
 yellow() {
 	echo -e "\033[33m${1}\033[0m"
 }
@@ -18,32 +12,38 @@ green() {
 red() {
 	echo -e "\033[31m${1}\033[0m"
 }
-check_debug_var() {
+if [[ -n $ASDEBUG ]]; then
 	if [[ "${ASDEBUG}" != "true" ]]; then
 		red "[E]: No ENV found. Exit."
 		exit 1
 	else
 		set -x
 	fi
-}
+fi
+# ENV
+X11APKURL="https://github.com/termux/termux-x11/releases/download/latest/app-arm64-v8a-debug.apk"
+PROJREPO="https://github.com/AtopesSayuri/TermuxX11AutoConfig"
+PROJRAWURL="https://github.com/AtopesSayuri/TermuxX11AutoConfig/raw/main"
+# Often used func
 print_help() {
 	blue "Termux X11 Auto Config"
 	blue "By AtopesSayuri"
 	blue ""
 	blue "Usage:"
-	blue "  -V,        verbose mode(need environment var. Check ${PROJREPO})."
 	blue "  -h,        print this help."
 	blue ""
 }
-
+pause() {
+	echo -e "\033[34m"
+	read -p "[I]: Press enter to continue."
+	echo -e "\033[0m"
+}
 # Arg solver
-while getopts ":hV" OPT; do
+while getopts ":h" OPT; do
 	case $OPT in
 	h)
 		print_help
-		;;
-	V)
-		check_debug_var
+        exit 0
 		;;
 	:)
 		red "[E]: Option -$OPTARG requires an argument." >&2 && exit 1
@@ -54,9 +54,7 @@ while getopts ":hV" OPT; do
 		;;
 	esac
 done
-
 #                              INFO: File Main Section
-
 #               INFO: check packages
 while [[ $CHECK -ne 2 ]]; do
 	CHECK=0
@@ -76,10 +74,50 @@ while [[ $CHECK -ne 2 ]]; do
 	fi
 	if (! command -v termux-x11 >/dev/null 2>&1); then
 		yellow "[W]: Termux:X11 package is not installed in Termux environment. Installing now..."
+		pkg up -y
+		pkg install x11-repo tur-repo -y
 		pkg install termux-x11-nightly -y
+		pkg install mesa-zink virglrenderer-mesa-zink vulkan-loader-android -y
 	else
 		CHECK=$((CHECK + 1))
 	fi
 done
 #               INFO:start config
-
+while true; do
+	blue "[I]: Select the type of container to use[R: root|P: proot]: "
+	read CONTAINERTYPE
+	case $CONTAINERTYPE in
+	R)
+		termux_starttx11_root
+		break
+		;;
+	P)
+		termux_starttx11
+		break
+		;;
+	*)
+		red "[E]: "
+		pause
+		;;
+	esac
+done
+termux_starttx11_root() {
+	cat <<-'EOF' >${PREFIX}/bin/starttx11-root
+		#!/bin/bash
+		    if [[ -z $* ]]; then
+		        echo "You need to provide the path to the container root directory eg /path/to/container"
+		        exit 1
+		    fi
+		    CONTAINERP=$1
+		export TMPDIR=${CONTAINERP}/tmp
+		export XKB_CONFIG_ROOT=${CONTAINERP}/usr/share/X11/xkb
+		export XDG_RUNTIME_DIR=${TMPDIR}
+		export CLASSPATH=$(/system/bin/pm path com.termux.x11 | cut -d: -f2)
+		su -c "/system/bin/app_process / com.termux.x11.CmdEntryPoint :0"
+	EOF
+	chmod +x ${PREFIX}/bin/starttx11-root
+}
+termux_starttx11() {
+	blue "[I]: Developing... Now exit."
+	exit 0
+}
